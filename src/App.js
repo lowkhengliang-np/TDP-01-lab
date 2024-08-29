c7b8214b8d7e993ec984d2bc1744847f5a9882d7
Kheng Liang <151629733+LowKhengLiang@users.noreply.github.com>
2024-08-29 09:31:18 +0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
+// import logo from './Chocolate-Lava-Cake-Blog1.jpg';
 import './App.css';
+// import './japfood.css'
+import JapFood from './Japfood.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+
+        <JapFood/>
+        
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
---
+++ src/JapFoodMenu.js
@@ -0,0 +1,14 @@
+function JapFoodMenu() {
+    return <div>
+        <nav>
+            <ul id="menu">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Our Menu</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+        </nav>
+    </div>
+}
+
+export default JapFoodMenu;
